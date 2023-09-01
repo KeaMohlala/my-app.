af7647d305b35e222a255ef11020e13c652e7959
KeaMohlala <137070041+KeaMohlala@users.noreply.github.com>
2023-09-01 14:53:20 +0200

    update app.js with weather html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+import "./styles.css";
+import Searchcity from "./Searchcity";
+import Searchbutton from "./Searchbutton";
+import Currentlocation from "./Currentlocation";
+import Forecast from "./Forecast";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <form className="search-form" id="search-form">
+          <div className="row">
+            <Searchcity />
+            <Searchbutton />
+            <Currentlocation />
+          </div>
+        </form>
+        <br />
+        <div className="border">
+          <div className="city">
+            <span id="cities">Johannesburg</span>
+            <span className="temperature" id="temp">
+              19
+            </span>
+            <span className="units">
+              <a href="#" id="celcius-link" className="active">
+                °C{" "}
+              </a>
+              |{" "}
+              <a href="#" id="farenheit-link">
+                °F{" "}
+              </a>
+            </span>
+          </div>
+          <div className="search" id="date">
+            Thursday, 11:37
+          </div>
+          <div className="icon">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+              alt="clear"
+              id="icon"
+            />
+          </div>
+          <div className="description" id="description">
+            Clear
+          </div>
+          <div className="moreinfo">
+            <span className="humidity" id="humidity">
+              Humidity: 0%
+            </span>
+            <span className="wind" id="wind">
+              Wind: 5km/h
+            </span>
+          </div>
+        </div>
+        <Forecast />
+        <small>
+          <a
+            href="https://github.com/KeaMohlala/finalweatherapp-shecodes"
+            target="_blank"
+          >
+            Open-source code
+          </a>
+          ,by Keamogetse Mohlala
+        </small>
+      </div>
     </div>
   );
 }
-
-export default App;

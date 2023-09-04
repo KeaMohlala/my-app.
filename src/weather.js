acec70bed3190b62dc4c8e1fb43078c3d3464e88
KeaMohlala <137070041+KeaMohlala@users.noreply.github.com>
2023-09-04 16:09:35 +0200

    fixed syntax error

--- src/weather.js
+++ src/weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       city: response.data.name,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon
+      icon: response.data.weather[0].icon,
       date: new Date(response.data.dt * 1000),
     });
   }
